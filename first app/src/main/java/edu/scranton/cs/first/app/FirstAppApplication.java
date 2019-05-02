cb1e71f0cb4c160080caaed730cf03caa5188cc4
Kylehill <Kyle.hill@scranton.edu>
2019-05-02 15:35:18 -0400

    IDK

--- first app/src/main/java/edu/scranton/cs/first/app/FirstAppApplication.java
+++ first app/src/main/java/edu/scranton/cs/first/app/FirstAppApplication.java
@@ -2,9 +2,17 @@ package edu.scranton.cs.first.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class FirstAppApplication {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FirstappApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstAppApplication.class, args);
